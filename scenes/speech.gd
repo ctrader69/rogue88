36af6977ed5fdc1d98f2f0a8741009da93965f2c
Christopher Trader <christopher.e.trader@gmail.com>
2025-03-22 10:14:23 -0400

    Speech improvements

---
+++ scenes/speech.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+const WRITE_DURATION = 0.25
+const FADE_DURATION = 2.0
+@export var text = 'default-text'
+var state = 'writing'
+
+func init(d):
+	if 'text' in d:
+		text = d['text']
+		
+func state_set(state):
+	match state:
+		'writing' :
+			$Label.text = ""
+			var tw = get_tree().create_tween()
+			tw.tween_method(update_text, 0, text.length(), WRITE_DURATION)
+			tw.tween_callback(self.state_set.bind("fading"))
+		'fading' :
+			var tw = get_tree().create_tween()
+			tw.tween_property(self, "position", position + Vector2(0, -8), FADE_DURATION)
+			tw.parallel()
+			tw.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 0.0), FADE_DURATION)
+			tw.tween_callback(self.state_set.bind("die"))
+		'die' :
+			queue_free()
+	
+func _ready():
+	state_set('writing')
+		
+func update_text(i):
+	$Label.text = text.substr(0, int(i))
+		
+func _process(delta):
+	# scroll/fade text
+	pass
+		
